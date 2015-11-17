190fe37444f0e9dc318f711e1a05d38218e5a8fc
Vaibhav Agrawal <vaibhavmagarwal@gmail.com>
2015-11-17 17:47:02 +0530

    Initial Commit.

---
+++ instagram.py
@@ -0,0 +1,82 @@
+import datetime
+import operator
+import requests
+import sys
+
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+ACCESS_TOKEN = 'xxxxxx.xxxxx.xxxxxxx'
+FOLLOWERS_ROOT = "https://api.instagram.com/v1/users/%d/followed-by?access_token=%s"
+LAST_POST_ROOT = "https://api.instagram.com/v1/users/%d/media/recent/?access_token=%s&count=1"
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+def get_followers(user_id):
+    '''
+    Returns the followers for a given Instagram user.
+    '''
+    response = requests.get(FOLLOWERS_ROOT % (int(user_id), ACCESS_TOKEN)).json()
+    if "data" in response and response["data"]:
+        return response["data"]
+
+    return None
+
+def last_post_time(user_id):
+    '''
+    Returns the time of the last post for a given Instagram user.
+    '''
+    response = requests.get(LAST_POST_ROOT % (int(user_id), ACCESS_TOKEN)).json()
+    if "data" in response and response["data"]:
+        return response["data"][0]["created_time"]
+
+    return None
+
+def get_day_from_timestamp(timestamp):
+    '''
+    Returns day of the week for a given timestamp.
+    '''
+    return datetime.datetime.fromtimestamp(int(timestamp)).strftime('%A')
+
+def get_hour_from_timestamp(timestamp):
+    '''
+    Returns hour of the day for a given timestamp
+    '''
+    return datetime.datetime.fromtimestamp(int(timestamp)).strftime('%H')
+
+@app.route("/process", methods=["GET"])
+def main():
+    DAY_COUNT = {}
+    HOUR_COUNT = {}
+    user_id = int(request.args.get('userid'))
+    username = request.args.get('username')
+    followers_list = get_followers(user_id)
+    if followers_list is None:
+        return "Cannot fetch information for the user as it is not available."
+
+    for follower in followers_list:
+        timestamp = last_post_time(follower["id"])
+        if not timestamp:
+            # skipping this user
+            continue
+
+        last_post_day = get_day_from_timestamp(timestamp)
+        if last_post_day not in DAY_COUNT:
+            DAY_COUNT[last_post_day] = 0
+
+        DAY_COUNT[last_post_day] += 1
+        last_post_hour = get_hour_from_timestamp(timestamp)
+        if last_post_hour not in HOUR_COUNT:
+            HOUR_COUNT[last_post_hour] = 0
+
+        HOUR_COUNT[last_post_hour] += 1
+
+
+    most_active_day = max(DAY_COUNT.iteritems(), key=operator.itemgetter(1))[0]
+    most_active_hour = int(max(HOUR_COUNT.iteritems(), key=operator.itemgetter(1))[0]) + 1
+    return render_template("response.html", time=most_active_hour, day=most_active_day, hour_count=HOUR_COUNT, day_count=DAY_COUNT)
+
+
+if __name__ == "__main__": app.run()
